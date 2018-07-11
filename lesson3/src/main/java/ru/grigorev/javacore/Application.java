07bdbe8eadd5c1ddd7b73b162e78246cfad3e9cc
meltiser <dmitriy-95@yandex.ru>
2018-07-11 14:24:22 +0300

    The first point of hometask

--- lesson3/src/main/java/ru/grigorev/javacore/Application.java
+++ lesson3/src/main/java/ru/grigorev/javacore/Application.java
@@ -1,10 +1,31 @@
 package ru.grigorev.javacore;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author Dmitriy Grigorev
+ * The first point of hometask
  */
 public class Application {
-    public static void main(String[] args) {
+    private static final String[] WORDS_ARRAY = {
+            "London", "is", "the", "capital", "of", "Great", "Britain",
+            "make", "Great", "Britain", "great", "again",
+            "Is", "London", "the", "capital"};
 
+    public static void main(String[] args) {
+        printUniqueWordsCount();
+    }
+
+    private static void printUniqueWordsCount() {
+        Map<String, Integer> map = new HashMap<>();
+        for (String s : WORDS_ARRAY) {
+            String word = s.toLowerCase();
+            if (map.containsKey(word)) map.put(word, map.get(word) + 1); //если содержит слово - увеличиваем счетчик
+            else map.put(word, 1);
+        }
+        for (Map.Entry<String, Integer> mapEntry : map.entrySet()) {
+            System.out.println(String.format("%-8s : %d", mapEntry.getKey(), mapEntry.getValue()));
+        }
     }
 }
