35813de944fade58d4ade93c432e6846b0f4aeb7
meltiser <dmitriy-95@yandex.ru>
2018-07-14 14:25:18 +0300

    checking version of java

---
+++ lesson4/src/main/java/ru/grigorev/javacore/Application.java
@@ -0,0 +1,15 @@
+package ru.grigorev.javacore;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Dmitriy Grigorev
+ */
+public class Application {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7));
+        numbers.forEach(e -> System.out.println(e));
+    }
+}
